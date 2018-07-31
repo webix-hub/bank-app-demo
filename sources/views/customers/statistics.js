f055223083e7e1f8c9b9c1dbd3a88ed0df34c528
Nastya <akovalenko@xbsoftware.com>
2018-07-31 20:54:10 +0300

    [fix] uncomment geochart

--- sources/views/customers/statistics.js
+++ sources/views/customers/statistics.js
@@ -11,7 +11,7 @@ export default class StatisticsView extends JetView {
 			type:"wide", cols:[
 				{
 					type:"wide", rows:[
-						{gravity:2},//GeoView,
+						GeoView,
 						MoneyView
 					]
 				},
