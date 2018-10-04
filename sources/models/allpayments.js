e460a7d3467e59753ecfe8b3fd8091bf45929ee0
Nastya <akovalenko@xbsoftware.com>
2018-10-04 13:36:51 +0300

    [add] dates now depend on the present

--- sources/models/allpayments.js
+++ sources/models/allpayments.js
@@ -1,8 +1,16 @@
 export const allpayments = new webix.DataCollection({
 	url:"data/payments.json",
-	scheme:{                                                                              
-		$init:function(obj){                                                                          
+	scheme:{
+		$init:function(obj){
 			obj.date = webix.i18n.parseFormatDate(obj.date);
-		}                                                                                  
+			const curr_month = new Date().getMonth();
+			const data_month = obj.date.getMonth();
+			if ((curr_month - data_month > 0) && (obj.id < 25)){
+				obj.date.setMonth(obj.date.getMonth()+1);
+			}
+			else if ((curr_month - data_month > 1) && (obj.id >= 25)){
+				obj.date.setMonth(obj.date.getMonth()+1);
+			}
+		}
 	}
 });
