55c0af2dcb9eae0ea8090053df4c362fccff3884
Nastya <akovalenko@xbsoftware.com>
2018-10-04 14:54:59 +0300

    [fix] updating dates

--- sources/models/allpayments.js
+++ sources/models/allpayments.js
@@ -5,11 +5,11 @@ export const allpayments = new webix.DataCollection({
 			obj.date = webix.i18n.parseFormatDate(obj.date);
 			const curr_month = new Date().getMonth();
 			const data_month = obj.date.getMonth();
-			if ((curr_month - data_month > 0) && (obj.id < 25)){
-				obj.date.setMonth(obj.date.getMonth()+1);
-			}
-			else if ((curr_month - data_month > 1) && (obj.id >= 25)){
-				obj.date.setMonth(obj.date.getMonth()+1);
+			if (curr_month - data_month > 0){
+				if (obj.id < 25)
+					obj.date.setMonth(curr_month);
+				else if (obj.id >= 25)
+					obj.date.setMonth(curr_month-1);
 			}
 		}
 	}
